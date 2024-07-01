a8cdae4c76246b0accb854773edcf40e09b6f1a3
Maicon Costa <maiconscosta@gmail.com>
2024-07-01 18:26:15 -0300

    just pass requestInfo as parameter to DualWriter
    Signed-off-by: Maicon Costa <maiconscosta@gmail.com>

--- pkg/registry/apis/dashboard/register.go
+++ pkg/registry/apis/dashboard/register.go
@@ -5,6 +5,7 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
+	k8srequest "k8s.io/apiserver/pkg/endpoints/request"
 	"k8s.io/apiserver/pkg/registry/generic"
 	"k8s.io/apiserver/pkg/registry/rest"
 	genericapiserver "k8s.io/apiserver/pkg/server"
@@ -142,7 +143,14 @@ func (b *DashboardsAPIBuilder) GetAPIGroupInfo(
 			return nil, err
 		}
 
-		storage[resourceInfo.StoragePath()], err = dualWriteBuilder(resourceInfo.GroupResource(), legacyStore, store, grafanarest.Mode1, reg, v0alpha1.GROUP, "dashboards", b.namespacer)
+		requestInfo := &k8srequest.RequestInfo{
+			APIGroup:  v0alpha1.GROUP,
+			Resource:  "dashboards",
+			Name:      "",
+			Namespace: b.namespacer(int64(1)),
+		}
+
+		storage[resourceInfo.StoragePath()], err = dualWriteBuilder(resourceInfo.GroupResource(), legacyStore, store, grafanarest.Mode1, reg, requestInfo)
 		if err != nil {
 			return nil, err
 		}
