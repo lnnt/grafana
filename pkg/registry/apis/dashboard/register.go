24b05bdcd251d5e879429230a68f9c1436fd1772
Maicon Costa <maiconscosta@gmail.com>
2024-07-02 18:36:29 -0300

    Add ServerLock mechanism
    Signed-off-by: Maicon Costa <maiconscosta@gmail.com>

--- pkg/registry/apis/dashboard/register.go
+++ pkg/registry/apis/dashboard/register.go
@@ -150,7 +150,7 @@ func (b *DashboardsAPIBuilder) GetAPIGroupInfo(
 			Namespace: b.namespacer(int64(1)),
 		}
 
-		storage[resourceInfo.StoragePath()], err = dualWriteBuilder(resourceInfo.GroupResource(), legacyStore, store, grafanarest.Mode1, reg, requestInfo)
+		storage[resourceInfo.StoragePath()], err = dualWriteBuilder(resourceInfo.GroupResource(), legacyStore, store, grafanarest.Mode1, reg, requestInfo, nil)
 		if err != nil {
 			return nil, err
 		}
